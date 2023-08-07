7f89395861215513c9fb0409d3e17abeacc66927
himanshu thambar <115807329+thambar@users.noreply.github.com>
2023-08-07 20:58:57 +0530

    Add files via upload

---
+++ airtable.py
@@ -0,0 +1,28 @@
+import requests
+
+AIRTABLE_API_KEY = 'your_airtable_api_key'
+AIRTABLE_BASE_ID = 'your_airtable_base_id'
+
+def copy_task_to_airtable(task_data):
+    url = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/Asana%20Tasks'
+    headers = {
+        'Authorization': f'Bearer {AIRTABLE_API_KEY}',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'fields': {
+            'Task ID': task_data['id'],
+            'Name': task_data['name'],
+            'Assignee': task_data['assignee']['name'] if task_data.get('assignee') else '',
+            'Due Date': task_data['due_on'] if task_data.get('due_on') else '',
+            'Description': task_data['notes'] if task_data.get('notes') else ''
+        }
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+
+    if response.status_code == 200:
+        print('Task copied to Airtable successfully!')
+    else:
+        print('Failed to copy task to Airtable.')
+
