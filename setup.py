18b24b1b96454ac8f7d4f0fbebbb53d599c63c03
himanshu thambar <115807329+thambar@users.noreply.github.com>
2023-08-08 14:52:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="integrating-asana-and-airtable",
     version="0.1",
     packages=find_packages(),
+    long_description_content_type='text/x-rst',
     install_requires=[
         # List your project dependencies here
     ],
